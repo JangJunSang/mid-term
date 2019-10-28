29c7824551080d77d729a4691e63b0397ba4b90d
JangJunSang <jjsj5971@gmailcom>
2019-10-28 10:15:18 +0900

    201921020-midterm-2

---
+++ 201921020-midterm-2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+  int i,j;
+  int *pi;
+
+  i=*pi;
+    
+  for(i=0;i<100;i++)
+  {
+    for(j=0;j<10;j++)
+    {
+        *pi++;
+    }
+    printf("\n");
+  }
+  printf("%d",i);
+}
